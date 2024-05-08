8ef09c87ea487b9c47d0d582d778ab80f3265536
Louiza Mak <louiza.mak@gmail.com>
2024-05-08 16:39:24 -0500

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,39 +2,93 @@ class NationalPark:
 
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if hasattr(self, 'name'):
+            raise Exception
+        if isinstance(name, str) and len(name) >= 3:
+            self._name = name
+        else:
+            raise Exception
         
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.national_park == self]
     
     def visitors(self):
-        pass
+        return list(set([trip.visitor for trip in self.trips()]))
     
     def total_visits(self):
-        pass
+        return len(self.trips())
     
     def best_visitor(self):
-        pass
-
+        visitors_list = tuple([trip.visitor for trip in self.trips()])
+        max_count = 0
+        best_visitor = None
+        for visitor in self.visitors():
+            if visitors_list.count(visitor) > max_count:
+                best_visitor = visitor
+        return best_visitor
 
 class Trip:
     
+    all = []
+
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+        Trip.all.append(self)
+
+    @property
+    def start_date(self):
+        return self._start_date
+    
+    @start_date.setter
+    def start_date(self, start_date):
+        if isinstance(start_date, str) and len(start_date) >= 7:
+            self._start_date = start_date
+        else: 
+            raise Exception
+        
+    @property
+    def end_date(self):
+        return self._end_date
+    
+    @end_date.setter
+    def end_date(self, end_date):
+        if isinstance(end_date, str) and len(end_date) >= 7:
+            self._end_date = end_date
+        else: 
+            raise Exception
 
 
 class Visitor:
 
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
+        else: 
+            raise Exception
         
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.visitor == self]
     
     def national_parks(self):
-        pass
+        return list(set([trip.national_park for trip in self.trips()]))
     
     def total_visits_at_park(self, park):
         pass
